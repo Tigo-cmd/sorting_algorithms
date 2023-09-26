7ef6653d9245a14770c5a79a855f8055bd2b5ca4
Tigo-cmd <ja16162040@gmail.com>
2023-09-26 08:28:47 -0400

    Quick sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,13 @@
+#include "sort.h"
+/**
+* 3-quick_sort.c - 
+* @param
+* @param
+*
+* Decsription: 
+* Return: Always(0) success
+*/
+void quick_sort(int *array, size_t size);
+{
+	
+}
--- sort.h
+++ sort.h
@@ -23,4 +23,5 @@ void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void swap(int *a, int *b);
+void quick_sort(int *array, size_t size);
 #endif
