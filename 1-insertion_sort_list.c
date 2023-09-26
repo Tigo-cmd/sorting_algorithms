acdae4e9be99ba7299629d85e181d218abb73041
Tigo-cmd <ja16162040@gmail.com>
2023-09-26 05:49:19 -0400

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,13 +1,39 @@
 #include "sort.h"
 /**
-* 1-insertion_sort_list.c - 
-* @param
-* @param
-*
-* Decsription: 
-* Return: Always(0) success
+* insertion_sort_list -  sorts a doubly linked list of integers
+* in ascending order using the Insertion sort algorithm
+* @list: Double linked list to sort
+* Return: nothing
 */
 void insertion_sort_list(listint_t **list);
 {
-	
+	listint_t *norm, *bef, *tmp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	{
+		return;
+	}
+
+	norm = (*list)->next;
+	while (norm)
+	{
+		bef =  norm->bef;
+		tmp = norm;
+		while (bef && bef->n > tmp->n)
+		{
+			if (bef->bef != NULL)
+				bef->bef->next = tmp;
+			if (tmp->next != NULL)
+				tmp->next->bef = bef;
+			bef->next = tmp->next;
+			tmp->bef = bef->bef;
+			tmp->next = bef;
+			bef->bef = tmp;
+			if (tmp->bef == NULL)
+				*list = tmp;
+			print_list((const listint_t *)*list);
+			bef = tmp->bef;
+		}
+		norm = norm->next;
+	}
 }
