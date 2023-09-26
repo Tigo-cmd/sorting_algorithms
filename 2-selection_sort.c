7e9fecfcdb668d9989edf180e2ddaf7c35340a8b
Tigo-cmd <ja16162040@gmail.com>
2023-09-26 06:42:56 -0400

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,13 +1,40 @@
 #include "sort.h"
 /**
-* 2-selection_sort.c - 
-* @param
-* @param
+* selection_sort - implements the selection sort algorithim 
+* @array: array to be sorted
+* @size: no. of items in the array
 *
-* Decsription: 
-* Return: Always(0) success
+* Return: Nothing
 */
-void selection_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size)
 {
-	
+	size_t j, i;
+	int temp, swaped, flag;
+
+	flag = 0;
+	if (!array)
+		return;
+	while (i < size)
+	{
+		temp = i;
+		flag = 0;
+		j = 1;
+		while ( j < size)
+		{
+			if (array[temp] > array[j])
+			{
+				temp = j;
+				flag = flag + 1;
+			}
+			j++;
+		}
+		swaped = array[i];
+		array[i] = array[temp];
+		array[temp] = swaped;
+		if (flag != 0)
+		{
+			print_array(array, size);
+		}
+		i++;
+	}
 }
