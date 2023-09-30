4a758653b7e65b6e87c6727229dd9e1b8a1d6d16
Tigo-cmd <ja16162040@gmail.com>
2023-09-29 21:40:48 -0400

    countimg sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,13 +1,77 @@
 #include "sort.h"
 /**
-* 102-counting_sort.c - 
-* @param
-* @param
+* _malloc_2 - implements memory alocations as calloc
+* @element: elements numbers
+* @size: size of each element bit
 *
-* Decsription: 
-* Return: Always(0) success
+* Return: pointer
+*/
+void *_malloc_2(unsigned int element, unsigned int size)
+{
+	char *call;
+	unsigned int i;
+
+	i = 0;
+	if (element == 0)
+		return ('\0');
+	if (size == 0)
+		return ('\0');
+	call = malloc(element * size);
+	if (call == '\0')
+		return ('\0');
+	while (i < (element * size))
+	{
+		call[i] = '\0';
+		i++;
+	}
+	return (call);
+}
+/**
+* counting_sort - function that sorts a doubly linked list of integers in
+* ascending order using the Cocktail shaker sort algorithm
+* @array: list of numbers
+* @size: size of the array
+*
+* Return: Nothing
 */
 void counting_sort(int *array, size_t size);
 {
-	
+	int i, max, *count, *temp;
+	size_t j;
+
+	count = '\0';
+	temp = '\0';
+	if (array == NULL)
+		return;
+	if (size < 2)
+		return;
+	j = 0;
+	while (j < size)
+	{
+		if (array[j] > max)
+			max = array[i];
+		j++;
+	}
+	count = _malloc_2(max + 1, sizeof(int));
+	temp = _malloc_2(size + 1, sizeof(int));
+	i = 1;
+	while (i <= max)
+	{
+		count[i] = count[i] + count[i - 1];
+		i++;
+	}
+	print_array(count, max + 1);
+	j = 0;
+	while (j < size)
+	{
+		temp[count[array[j]] - 1] = array[j];
+		count[array[j]] = count[array[i]] - 1;
+		j++;
+
+	}
+	for (j = 0; j < size; j++)
+		array[j] = temp[j];
+	free(temp);
+	free(count);
+}
 }
